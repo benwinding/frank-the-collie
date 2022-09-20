65f8b9a0e75aa01a8cdf2173db07b67e287035e9
Ben Winding <hello@benwinding.com>
2022-09-20 13:12:02 +1000

    Add link to blog post

--- src/components/Rip.svelte
+++ src/components/Rip.svelte
@@ -5,6 +5,8 @@
 </style>
 
 <div class="text-right">
-  <h1 class="bold">R.I.P</h1>
-  <p class="text-xs">You were a good boy</p>
+  <a href="https://blog.benwinding.com/frank/">
+    <h1 class="bold">R.I.P</h1>
+    <p class="text-xs">You were a good boy</p>
+  </a>
 </div>
